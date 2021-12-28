abbd9d5e5b458f1492166e26748a29533896552a
chuckboliver <patpumhak@gmail.com>
2021-12-28 23:46:32 +0700

    add !

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ intents = dc.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="?", intents=intents)
 
+
 @bot.event
 async def on_ready():
-    print("Started!!!")
+    print("Started!!!!")
 
 
 def show_server_list():
@@ -20,9 +21,11 @@ def show_server_list():
         print(f"{num}. Sv: {guild.name} ---> {guild.id}")
         num += 1
 
+
 @bot.event  # Event bot exit from guild
 async def on_guild_remove(guild):
     show_server_list()
+
 
 @bot.event  # Event bot join guild
 async def on_guild_join(guild):
@@ -33,17 +36,22 @@ async def on_guild_join(guild):
 async def dm(ctx, member: dc.Member, *, content: str):
     await member.send(content)
 
+
 @bot.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None or after.channel is not None:
         if after.channel.id == 702116420753948672:
-            ch = dc.utils.get(member.guild.text_channels, name='ดู-ส้มตำ-v2')
+            ch = dc.utils.get(member.guild.text_channels, name="ดู-ส้มตำ-v2")
             await ch.send("Hey!")
+
 
 @bot.command()
 async def clear(ctx, *, content: int):
     deleted = await ctx.message.channel.purge(limit=content)
-    await ctx.message.channel.send('Deleted {} message(s)'.format(len(deleted)),delete_after=3)
+    await ctx.message.channel.send(
+        "Deleted {} message(s)".format(len(deleted)), delete_after=3
+    )
+
 
 if __name__ == "__main__":
     load_dotenv()
